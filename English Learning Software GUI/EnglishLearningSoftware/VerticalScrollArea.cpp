a40c782bceea7498f67fd5331d3780e8c1043972
dotrann1412 <dotrann.1412@gmail.com>
2022-01-02 10:40:01 +0700

    Vertical scroll widget

---
+++ English Learning Software GUI/EnglishLearningSoftware/VerticalScrollArea.cpp
@@ -0,0 +1,44 @@
+#include "VerticalScrollArea.h"
+
+#include "ConstantGUI.h"
+#include <QSizePolicy>
+
+VerticalScrollArea::VerticalScrollArea(int cols, QWidget* parent)
+    : QScrollArea(parent),
+      nCols(cols) {
+
+    this->setWidgetResizable(true);
+    contentWidget = new QWidget(this);
+    this->setWidget(contentWidget);
+    contentWidget->setLayoutDirection(Qt::LeftToRight);
+
+    grid = new QGridLayout(contentWidget);
+
+    this->setVerticalScrollBarPolicy(Qt::ScrollBarAsNeeded);
+    this->setHorizontalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
+
+    this->setStyleSheet(ConstantGUI::VERTICAL_SCROLL_STYLE);
+
+    this->count = 0;
+}
+
+VerticalScrollArea::~VerticalScrollArea(){
+    delete this->grid;
+    delete this->contentWidget;
+}
+
+void VerticalScrollArea::addWidget(QWidget *w) {
+    if(!w)
+        return;
+
+    int row = count / nCols;
+    int col = count % nCols;
+    this->grid->addWidget(w, row, col);
+
+    if(!(count % nCols)) {
+        int height = w->height() * ((count + nCols - 1) / nCols + 1);
+        this->contentWidget->setFixedHeight(height + ConstantGUI::QUESTION_BUTTON_PADDING * ((count + nCols - 1) / nCols + 1));
+    }
+
+    this->count++;
+}
